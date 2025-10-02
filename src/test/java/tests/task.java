6ab15e0096de6814f8ec9fb1fd61157d77e8e803
leylacoskunkurt <leylacoskunng@gmail.com>
2025-10-02 14:47:57 +0200

    add void method

--- src/test/java/tests/task.java
+++ src/test/java/tests/task.java
@@ -5,4 +5,9 @@ public static void main(String[] args){
   System.out.println("Hello wold");
   //This line is added from GitHub, see you in IntelliJ
 }
+
+
+public void hi(){
+  System.out.println("Hi from IntelliJ");
+}
 }
