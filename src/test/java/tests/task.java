9e303d13e0a9b4f7bb143997baa49670350b55a8
leylacoskunkurt <leylacoskunng@gmail.com>
2025-10-02 14:50:35 +0200

    added return message

--- src/test/java/tests/task.java
+++ src/test/java/tests/task.java
@@ -4,5 +4,9 @@ public class task {
 public static void main(String[] args){
   System.out.println("Hello wold");
   //This line is added from GitHub, see you in IntelliJ
+  
+}
+public String hello(){
+  return "Hello from GitHub";
 }
 }
